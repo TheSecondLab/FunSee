a63e9a214591a4cbf3cff2ab7d8f4fd6ce3b61a7
MeatPieYan <robinyzg@hotmail.com>
2018-02-17 23:46:50 +0800

    add comment

--- config/defaultFunSeeConfig.js
+++ config/defaultFunSeeConfig.js
@@ -1,18 +1,32 @@
 /**
  * ------------------------------------------------------------------
- * middleware: [{
+ * port<number>: node启动端口号
+ * ------------------------------------------------------------------
+ * middleware<array of optionObj>: 配置FunSee所使用的第三方中间件
+ * optionObj: {  
  *  name<string>: 中间件名字, npm包名
  *  param<any || array>: 传入中间件的参数, 如果是array的话，将会把数组的每一项依次传入中间件
  *  needApp<bool>: 是否需要app传入中间件， 需要的话会再中间件的使用方法最后传入app
+*   desc<string>: 打印日志用
  * }]
+ * ------------------------------------------------------------------
+ *  customMiddleware<array of optionObj>: 配置FunSee所使用的自定义中间件
+ *  optionObj: {  
+ *   name<string>: 中间件名字, npm包名
+ *   param<any || array>: 传入中间件的参数, 如果是array的话，将会把数组的每一项依次传入中间件
+ *   needApp<bool>: 是否需要app传入中间件， 需要的话会再中间件的使用方法最后传入app，
+ *   desc<string>: 打印日志用, 不传则使用name代替
+ *  }]
  * ------------------------------------------------------------------
  */
 module.exports = {
   port: 8080,
   middleware: [{
-    name: require('koa-compress')
+    name: require('koa-compress'),
+    desc: 'koa-compress'
   }, {
     name: require('koa-views'),
+    desc: 'koa-views',
     param: [`${global.__ROOT_PATH__}/views`, {
       map: {
         html: 'ejs'
