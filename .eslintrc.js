5df8f990e1dd0f52395cfe6f00e8e48cf25febb3
MeatPieYan <robinyzg@hotmail.com>
2018-02-23 16:04:38 +0800

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  "extends": "funsee",
+  "rules": {
+    "global-require": ["off"]
+  }
+};
--- config/defaultFunSeeConfig.js
+++ config/defaultFunSeeConfig.js
@@ -3,7 +3,7 @@
  * port<number>: node启动端口号
  * ------------------------------------------------------------------
  * middleware<array of optionObj>: 配置FunSee所使用的第三方中间件
- * optionObj: {  
+ * optionObj: {
  *  name<string>: 中间件名字, npm包名
  *  param<any || array>: 传入中间件的参数, 如果是array的话，将会把数组的每一项依次传入中间件
  *  needApp<bool>: 是否需要app传入中间件， 需要的话会再中间件的使用方法最后传入app
@@ -11,7 +11,7 @@
  * }]
  * ------------------------------------------------------------------
  *  customMiddleware<array of optionObj>: 配置FunSee所使用的自定义中间件
- *  optionObj: {  
+ *  optionObj: {
  *   name<string>: 中间件名字, npm包名
  *   param<any || array>: 传入中间件的参数, 如果是array的话，将会把数组的每一项依次传入中间件
  *   needApp<bool>: 是否需要app传入中间件， 需要的话会再中间件的使用方法最后传入app，
